9058943a141b055a296d36aac8857d94ede08d43
Hideaki-Ryu <cenarahmant@gmail.com>
2023-10-05 05:02:29 +0700

    feat: add morgan for logging

--- middleware/index.ts
+++ middleware/index.ts
@@ -2,11 +2,13 @@ import { Application } from "express";
 import bodyParserMiddleware from "./bodyParserMiddleware";
 import helmetMiddleware from "./helmetMiddleware";
 import corsMiddleware from "./corsMiddleware";
+import morganMiddleware from "./morganMiddleware";
 
 const applyMiddleware = (app: Application) => {
   helmetMiddleware(app);
   bodyParserMiddleware(app);
   corsMiddleware(app);
+  morganMiddleware(app);
 };
 
 export default applyMiddleware;
