bbd3ba4ead478c0d8d1d65a8f8a2b095429f142a
Hideaki-Ryu <cenarahmant@gmail.com>
2023-10-04 17:46:44 +0700

    feat: apply middleware, database, & errorHandler

--- app.ts
+++ app.ts
@@ -1,18 +1,22 @@
 import "dotenv/config";
 import express, { Application } from "express";
-import bodyParser from "body-parser";
 import routes from "./routes";
-import cors from "cors";
+import middleware from "./middleware";
+import databaseMiddleware from "./middleware/databaseMiddleware";
+import errorHandlerMiddleware from "./middleware/errorHandleMiddleware";
 
 const app: Application = express();
 const PORT: number = parseInt(<string>process.env.PORT, 10) || 8080;
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// Use middlewares
+middleware(app);
+app.use(databaseMiddleware);
 
 // Use routes
 app.use(routes);
+
+// Use error handler
+app.use(errorHandlerMiddleware);
 
 // App listeners
 app.listen(PORT, () =>
