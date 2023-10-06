35dbf3f7591e290f9285c12e8e9c94884285febc
Hideaki-Ryu <cenarahmant@gmail.com>
2023-10-06 20:37:31 +0700

    feat(api): add preventAttack Api

---
+++ controller/preventAttackController.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+
+async function getDataXss(req: Request, res: Response, next: NextFunction) {
+  const { name } = req.query;
+  try {
+    const data = "ini data sensitive";
+    res.send(`Hello ${name}`);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getClickJacking(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    res.send(`
+           <form action="/click-jacking" method="post">
+             <label for="username">Username:</label><br>
+             <input type="text" id="username" name="username"><br>
+             <label for="password">Password:</label><br>
+             <input type="password" id="password" name="password"><br>
+             <input type="submit" value="Submit">
+           </form>
+         `);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function createClickJacking(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    let username = req.body.username;
+    let password = req.body.password;
+    res.json({ username: username, password: password });
+  } catch (error) {
+    next(error);
+  }
+}
+
+export { getDataXss, getClickJacking, createClickJacking };
